34d2a18d31c2bb45b5cdd736ce3df2f2e9b21e36
kofimeighan <km3547@columbia.edu>
2020-06-01 11:43:20 -0400

    First servlet file

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,9 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<p><b>Hello Kofi</b></p>");
   }
 }
