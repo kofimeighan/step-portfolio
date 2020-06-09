4ce6d16fa1d8901d7b9da589b3b74626dd73a5f7
Kofi Meighan <km3547@columbia.edu>
2020-06-09 14:25:16 +0000

    Added username instance variable.

--- portfolio/src/main/java/com/google/sps/servlets/Comments.java
+++ portfolio/src/main/java/com/google/sps/servlets/Comments.java
@@ -18,13 +18,16 @@ public final class Comments {
 
     private final long id;
     private final String name;
+    private final String username;
     private final String email;
     private final String message;
     private final long timeStamp;
+    
 
-    public Comments(long id, String name, String email, String message, long timeStamp) {
+    public Comments(long id, String name, String username, String email, String message, long timeStamp) {
         this.id = id;
         this.name = name;
+        this.username = username;
         this.email = email;
         this.message = message;
         this.timeStamp = timeStamp;
